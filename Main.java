45db4d3b2f1e575af1bf212f2aa249396cca44ff
afterkita <90507595+afterkita@users.noreply.github.com>
2023-10-06 18:27:19 +0300

    Лаб. работа 1

--- Main.java
+++ Main.java
@@ -1,178 +1,178 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    public static void Task1(int n){
+        int s = 0;
+        int m = n;
 
-    public static void main(String[] arg)
-    {
-        System.out.println((Arrays.deepToString(Task8(new int[][]{{1, 3, 5, 9}, {3,17,27,47}, {2, 4, 6, 5}}))));
-    }
-
-    /* 1. Задача: Найти наибольшую подстроку без повторяющихся символов.
-        Условие: Напишите метод, который принимает строку и возвращает наибольшую подстроку, в
-        которой все символы уникальны.*/
-    public static String Task1(String inputString)
-    {
-        String usedChars = "";
-
-        int start = 0;
-        
-        int max_start = 0;
-        
-        int max_lenght = 0;
-        
-        int lenght = 0;
-
-        for (int i = 0; i < inputString.length(); i++)
-        {
-            String Char = "" + inputString.charAt(i);
-            if (usedChars.contains(Char))
-            {
-                start = i;
-                usedChars = "";
+        while (n>1){
+            //System.out.println(n);
+            if (n%2 == 1){
+                n = (n*3)+1;
             }
-            usedChars = usedChars + Char;
-            lenght = i - start + 1;
-            if (lenght > max_lenght)
-            {
-                max_lenght = lenght;
-                max_start = start;
+            else {
+                n = n/2;
+            }
+            s += 1;
+            if (m >= n){
+                m = m;
+            }
+            else {
+                m = n;
             }
         }
 
-        return inputString.substring(max_start, max_start + max_lenght);
+        System.out.println("Шагов: "+s);
+
     }
-
-    /*2. Задача: Объединить два отсортированных массива.
-    Условие: Напишите метод, который принимает два отсортированных массива и возвращает
-    новый массив, содержащий все элементы из обоих массивов в отсортированном порядке.*/
-    public static int[] Task2(int[] arr1, int[] arr2)
-    {
-        int[] arr_op = new int[arr1.length + arr2.length];
-
-        int Index = 0;
-        for(int element : arr1)
-        {
-            arr_op[Index] = element;
-            Index += 1;
-        }
-        for(int element : arr2)
-        {
-            arr_op[Index] = element;
-            Index += 1;
-        }
-        Arrays.sort(arr_op);
-        return arr_op;
-    }
-
-    /*3. Задача: Найти максимальную сумму подмассива.
-    Условие: Напишите метод, который принимает массив целых чисел и возвращает максимальную
-    сумму подмассива (последовательных элементов).*/
-    public static int Task3(int[] arr) {
-        int maxSum = arr[0];
-        int sum_g = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            sum_g = Math.max(arr[i], sum_g + arr[i]);
-            maxSum = Math.max(maxSum, sum_g);
-        }
-        return maxSum;
-    }
-
-    /* 4. Задача: Повернуть массив на 90 градусов по часовой стрелке.
-    Условие: Напишите метод, который принимает двумерный массив и возвращает новый массив,
-    полученный путем поворота исходного массива на 90 градусов по часовой стрелке.*/
-    public static int[][] Task4(int[][] matrix_arr)
-    {
-        int rows = matrix_arr.length;
-        int cols = matrix_arr[0].length;
-        int[][] newmatrix_arr = new int[cols][rows];
-        for(int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                newmatrix_arr[j][rows - 1 - i] = matrix_arr[i][j];
-            }
-        }
-        return newmatrix_arr;
-    }
-
-    /* 4. Задача: Найти пару элементов в массиве, сумма которых равна заданному числу.
-    Условие: Напишите метод, который принимает массив целых чисел и целое число target и
-    возвращает пару элементов массива, сумма которых равна target. Если такая пара не существует,
-    метод должен вернуть null.*/
-    public static int[] getPair(int[] numbers, int target)
-    {
-        for(int i = 0; i < numbers.length - 1; i++)
-        {
-            for(int j = i + 1; j < numbers.length; j++)
-            {
-                if(numbers[i] + numbers[j] == target)
-                {
-                    return new int[]{numbers[i], numbers[j]};
-                }
-            }
-        }
-        return null;
-    }
-
-    /*6. Задача: Найти сумму всех элементов в двумерном массиве.
-    Условие: Напишите метод, который принимает двумерный массив целых чисел и возвращает их
-    сумму.*/
-    public static int Task6(int[][] matrix_arr)
-    {
+    public static void Task2(int n){
+        int k = n;
         int sum = 0;
-        for(int[] row : matrix_arr)
-        {
-            for(int element: row)
-            {
-                sum += element;
+
+        Scanner SCN = new Scanner(System.in);
+        for (int i = 0;i< k ; i++){
+            int val = SCN.nextInt();
+            if (i%2 == 0){
+                sum +=val;
+            }
+            else {
+                sum -= val;
             }
         }
-        return sum;
+        System.out.println("Результат: " + sum);
     }
 
-    /*7. Задача: Найти максимальный элемент в каждой строке двумерного массива.
-    Условие: Напишите метод, который принимает двумерный массив целых чисел и возвращает
-    одномерный массив, содержащий максимальный элемент из каждой строки исходного массива.*/
-    public static int[] Task7(int[][] matrix_arr)
-    {
-        int[] arr_op = new int[matrix_arr.length];
-        int index = 0;
+    public static void Task3(){
+        Scanner SCN = new Scanner(System.in);
+        int x_final = SCN.nextInt();
+        int y_final = SCN.nextInt();
 
-        for(int[] row : matrix_arr)
-        {
-            int maxElement = row[0];
 
-            for(int element : row)
-            {
-                if(element > maxElement)
-                {
-                    maxElement = element;
+        int x = 0;
+        int y = 0;
+        int step = 0;
+
+        boolean stoper = false;
+        System.out.printf("Введите начальные точки: \n");
+        while (stoper == false){
+            String line = SCN.nextLine();
+            if (line.equals("стоп")){
+                stoper = true;
+            } else if (line.equals("север")) {
+                int val = SCN.nextInt();
+                x += val;
+                step++;
+            }else if (line.equals("юг")) {
+                int val = SCN.nextInt();
+                x -= val;
+                step++;
+            }else if (line.equals("восток")) {
+                int val = SCN.nextInt();
+                y += val;
+                step++;
+            }else if (line.equals("запад")) {
+                int val = SCN.nextInt();
+                y -= val;
+                step++;
+            } else {
+                stoper = false;
+            }
+            if ((x == x_final) &&(y == y_final)){
+                stoper = true;
+            }
+            //System.out.println("x-"+x+" y-"+y);
+        }
+        System.out.println("Ответ: "+step);
+    }
+    public static void Task4(){
+        Scanner SCN = new Scanner(System.in);
+
+        int which_way = 1;
+        int stratig = 1;
+
+        int val = SCN.nextInt();
+        for (int i=0;i<val;i++){
+
+
+            int way_count = SCN.nextInt();
+            //System.out.println("---"+way_count);
+            int min_hight = 1000000;
+
+            for (int j =0;j<way_count;j++){
+                int way_hight = SCN.nextInt();
+                if (way_hight < min_hight){
+                    min_hight = way_hight;
                 }
             }
-            arr_op[index] = maxElement;
-            index++;
-        }
-
-        return arr_op;
-    }
-/*8. Задача: Повернуть двумерный массив на 90 градусов против часовой стрелке.
-Условие: Напишите метод, который принимает двумерный массив и возвращает новый массив,
-полученный путем поворота исходного массива на 90 градусов против часовой стрелке,*/
-    public static int[][] Task8(int[][] matrix_arr)
-    {
-        int rows = matrix_arr.length;
-        int cols = matrix_arr[0].length;
-
-        int[][] newmatrix_arr = new int[cols][rows];
-
-        for(int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                newmatrix_arr[cols - 1 - j][i] = matrix_arr[i][j];
+            if (min_hight > stratig){
+                which_way = i+1;
+                stratig = min_hight;
             }
         }
+        System.out.println(which_way+" "+stratig);
+    }
+    public static void Task5(){
+        Scanner SCN = new Scanner(System.in);
+        String line = SCN.nextLine();
 
-        return newmatrix_arr;
+        int val1 = 0;
+        int val2 = 1;
+        for (int i =0;i<line.length();i++){
+            char c = line.charAt(i);
+            String c1 = String.valueOf(c);
+            int gir = Integer.parseInt(c1);
+            val2 = val2*gir;
+            val1 = val1+gir;
+        }
+        if ((val1%2==0)&&(val2%2==0)){
+            System.out.println("Является дважды чётным");
+        }
+        else {
+            System.out.println("Не является дважды чётным");
+        }
+    }
+    public static void main(String[] args) {
+        // Метод введения массива небыл указан, так что массив менять тут >>
+        int[][] arr = {{14, 31, 323, 324},
+                {44, 35, 46, 234},
+                {57, 8, 9323, 32},
+                {213,321,4,234}};
+
+        int[] Row = new int[arr.length];
+        int[] Tab = new int[arr.length];
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr[i].length; j++)
+            {
+
+                Row[i] += arr[i][j];
+                Tab[j] += arr[i][j];
+
+            }
+        }
+        System.out.println(Arrays.toString(Row));
+        System.out.println(Arrays.toString(Tab));
+
+        Scanner SCN = new Scanner(System.in);
+        int val;
+
+        System.out.printf("Задача 1 'Сиракузкая последовательность' \nВведите число: ");
+        val = SCN.nextInt();
+        Task1(val); // Задача 1
+
+        System.out.printf("Задача 2 'Сумма ряда' \nВведите количество, а затем сами числа: ");
+        val = SCN.nextInt();
+        Task2(val); // Задача 2
+
+        System.out.printf("Задача 3 'Поиск клада'");
+        Task3(); // Задача 3
+
+        System.out.printf("Задача 4 'Логистический максимум'");
+        Task4(); // Задача 4
+
+        System.out.printf("Задача 5 'Дваждычётное число'");
+        Task5(); // Задача 5
     }
 }
